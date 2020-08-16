03fb0f50347fc7d5c305c2c3dd971d96a2e4b895
Crucian <daniilnikitin020405@gmail.com>
2020-08-16 12:08:20 +0600

    make car page

--- src/body.js
+++ src/body.js
@@ -8,9 +8,64 @@ class Body extends React.Component {
 	render(){
 		return(
 		<div className="grid-x grid-padding-x">
-  <div className="cell medium-8 large-8">12/6/4 cells</div>
-  <div className="cell medium-4 large-4">12/6/8 cells</div>
-</div>
+            <div className="cell medium-3 large-3 text-left">
+             <ul>
+             <li>
+             <a href ="">BMW</a>
+             </li>
+             <li>
+             <a href ="">Mercedes</a>
+             </li>
+             <li>
+             <a href ="">Ford</a>
+             </li>
+             <li>
+             <a href ="">Ваз</a>
+             </li>
+             <li>
+             <a href ="">Toyota</a>
+             </li>
+             </ul>
+             </div>
+            <div className="cell medium-8 large-8">
+            <div className="grid-container">
+                 <div className="grid-x grid-margin-x small-up-2 medium-up-3">
+                    <div className="cell">
+                <div className="card">
+                     <div className="card-section">
+                       <p>Старый элитный Bmw седан.</p>
+                      </div>
+                      <img src="../images/mark/BMW/cattouch.jpg"/>
+                </div>
+                </div>
+                <div className="cell">
+                <div className="card">
+                     <div className="card-section">
+                       <p>Bmw x3</p>
+                      </div>
+                      <img src="../images/mark/BMW/1200px-2018_BMW_X3_xDrive30d_M_Sport_Automatic_3.0_Front.jpg"/>
+                </div>
+                </div>
+                <div className="cell">
+                <div className="card">
+                     <div className="card-section">
+                       <p>новый седан</p>
+                      </div>
+                      <img src="../images/mark/BMW/bmw2grancoupe.jpg"/>
+                </div>
+                </div>
+                <div className="cell">
+                <div className="card">
+                     <div className="card-section">
+                       <p>супер Новый седан</p>
+                      </div>
+                      <img src="../images/mark/BMW/p90385005-highres.jpg"/>
+                </div>
+                </div>
+                </div>
+                </div>
+            </div>
+        </div>
 		);
 	}
 }
