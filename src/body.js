53fbea88fff3e63591d6c8702443f887c244f893
Crucian <daniilnikitin020405@gmail.com>
2020-08-23 11:52:01 +0600

    Change body.js

--- src/body.js
+++ src/body.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Table_car from './table_car.js';
 
 class Body extends React.Component {
 	constructor(props){
@@ -6,66 +7,42 @@ class Body extends React.Component {
 	}
 
 	render(){
+		
+		const car_list = [
+			{model: "Старый седан", img: "../images/mark/BMW/3.jpg"},
+			{model: "X3", img: "../images/mark/BMW/2.jpg"},
+			{model: "Внедорожник", img: "../images/mark/BMW/1.jpg"},
+			{model: "Новая машина", img: "../images/mark/BMW/4.jpg"},
+			
+		]
+		
 		return(
 		<div className="grid-x grid-padding-x">
             <div className="cell medium-3 large-3 text-left">
-             <ul>
-             <li>
-             <a href ="">BMW</a>
-             </li>
-             <li>
-             <a href ="">Mercedes</a>
-             </li>
-             <li>
-             <a href ="">Ford</a>
-             </li>
-             <li>
-             <a href ="">Ваз</a>
-             </li>
-             <li>
-             <a href ="">Toyota</a>
-             </li>
-             </ul>
-             </div>
-            <div className="cell medium-8 large-8">
-            <div className="grid-container">
-                 <div className="grid-x grid-margin-x small-up-2 medium-up-3">
-                    <div className="cell">
-                <div className="card">
-                     <div className="card-section">
-                       <p>Старый элитный Bmw седан.</p>
-                      </div>
-                      <img src="../images/mark/BMW/cattouch.jpg"/>
-                </div>
-                </div>
-                <div className="cell">
-                <div className="card">
-                     <div className="card-section">
-                       <p>Bmw x3</p>
-                      </div>
-                      <img src="../images/mark/BMW/1200px-2018_BMW_X3_xDrive30d_M_Sport_Automatic_3.0_Front.jpg"/>
-                </div>
-                </div>
-                <div className="cell">
-                <div className="card">
-                     <div className="card-section">
-                       <p>новый седан</p>
-                      </div>
-                      <img src="../images/mark/BMW/bmw2grancoupe.jpg"/>
-                </div>
-                </div>
-                <div className="cell">
-                <div className="card">
-                     <div className="card-section">
-                       <p>супер Новый седан</p>
-                      </div>
-                      <img src="../images/mark/BMW/p90385005-highres.jpg"/>
-                </div>
-                </div>
-                </div>
-                </div>
-            </div>
+				<ul>
+					<li>
+						<a href ="">BMW</a>
+					</li>
+					<li>
+						<a href ="">Mercedes</a>
+					</li>
+					<li>
+						<a href ="">Ford</a>
+					</li>
+					<li>
+						<a href ="">Ваз</a>
+					</li>
+					<li>
+						<a href ="">Toyota</a>
+					</li>
+				</ul>
+			</div>
+			
+			<div className="cell medium-8 large-8">
+           <Table_car mark="BMWs" cars={car_list} />
         </div>
+			
+		</div>
 		);
 	}
 }
